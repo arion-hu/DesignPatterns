b1825c93657fce645e817426f045c155de768373
阿川 胡 <huachuan@sh.zpmc.com>
2019-12-19 14:35:26 +0800

    first commit

---
+++ SingletonPatterns/Singleton.cpp
@@ -0,0 +1,11 @@
+#include "Singleton.h"
+
+Singleton* Singleton::m_pSingleton = NULL;
+
+Singleton* Singleton::GetInstance()
+{
+	if (m_pSingleton == NULL) {
+		m_pSingleton = new Singleton();
+	}
+	return m_pSingleton;
+}
